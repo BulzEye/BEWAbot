af2f8d9ee58fd4dc7f760d4f98b7e4a287aa5de6
BulzEye <shirgaonkarom@yahoo.in>
2021-07-23 05:31:03 +0530

    v1.2: added modes like sticker to image, and more

--- index.js
+++ index.js
@@ -29,9 +29,9 @@ async function connectWA() {
                 // console.log(msg);
                 // console.log(msgType);
 
-                if(msgType === MessageType.text || msgType === MessageType.extendedText) {
+                if(msgType === MessageType.text) {
                     // console.log(msg);
-                    const msgText = (msgType === MessageType.text) ? msg.message.conversation : msg.message.extendedTextMessage.text;
+                    const msgText = msg.message.conversation;
                     if(msgText.startsWith("!")) {
                         // console.log("triggered");
                         const cmd = msgText.split(" ")[0].substring(1);
@@ -44,6 +44,23 @@ async function connectWA() {
                             const toSend = msg.key.remoteJid;
                             const response = await conn.sendMessage(toSend, "Hello, World!", MessageType.text);
                         }
+                    }
+                    if(msgText.toLowerCase().trim() === "gn gays") {
+                        let kickMember = msg.participant;
+                        conn.groupRemove(msg.key.remoteJid, [kickMember]).then((modi) => {
+                            console.log("Removed people.");
+                            console.log("Data received: " + modi);
+                            setTimeout(() => {
+                                conn.groupAdd(msg.key.remoteJid, [kickMember]).then((modi) => {
+                                    console.log("Added people back.");
+                                    console.log("Data received: " + modi);
+                                }).catch((err) => {
+                                    console.log("ERROR in adding member: " + err);
+                                });
+                            }, 60000);
+                        }).catch((err) => {
+                            console.log("ERROR in removing member: " + err);
+                        });
                     }
                 }
 
@@ -86,7 +103,7 @@ async function connectWA() {
                                 //         });
                                 //     }
                                 // });  
-                                conn.sendMessage(msg.key.remoteJid, "BEWAbot: GIF conversion not supported yet! Only regular images supported.").then((response) => {
+                                conn.sendMessage(msg.key.remoteJid, "BEWAbot: GIF conversion not supported yet! Only regular images supported.", MessageType.text).then((response) => {
                                     console.log("Animated sticker request declined as WIP");
                                 })   
                             }
@@ -120,9 +137,10 @@ async function connectWA() {
                     if(msgText.startsWith("!")) {
                         const cmd = msgText.split(" ")[0].substring(1);
                         let cmdContent = msgText.substring(msgText.indexOf(" ") + 1);
+                        // console.log(cmd);
                         switch(cmd) {
                             case "sticker":
-                                conn.sendMessage(msg.key.remoteJid, "BEWAbot: Tagged sticker requests are currently not supported! Send the image again using the command in the caption.").then((response) => {
+                                conn.sendMessage(msg.key.remoteJid, "*BEWAbot:* Tagged sticker requests are currently not supported! Send the image again using the command in the caption.", MessageType.text).then((response) => {
                                     console.log("Tag sticker message received, sent WIP message.");
                                 }).catch(msgSendError);
                                 break;
@@ -142,7 +160,94 @@ async function connectWA() {
                                     }).catch(msgSendError);
                                 }
                                 else {
-                                    conn.sendMessage(msg.key.remoteJid, "BEWAbot: Tag a text message!").then((response) => console.log("Message rejected: Non-text message tagged")).catch(msgSendError);
+                                    conn.sendMessage(msg.key.remoteJid, "*BEWAbot:* Tag a text message!", MessageType.text).then((response) => console.log("Message rejected: Non-text message tagged")).catch(msgSendError);
+                                }
+                                break;
+
+                            case "kiddoShit":
+                                let taggedMsgType1 = Object.keys(msg.message.extendedTextMessage.contextInfo.quotedMessage)[0];
+                                if(taggedMsgType1 === MessageType.text) {
+                                    console.log("Received kiddo conversion request");
+                                    let conText = msg.message.extendedTextMessage.contextInfo.quotedMessage.conversation;
+                                    // console.log(conText);
+                                    let arText = [];
+                                    for(let i=0; i<conText.length; i++) {
+                                        let ch = conText.charAt(i);
+                                        let chNew = ' '
+                                        switch(ch) {
+                                            case 'r':
+                                                chNew = 'l';
+                                                break;
+
+                                            case 'j':
+                                                chNew = 'd';
+                                                break;
+
+                                            case 'l':
+                                                chNew = 'r';
+                                                break;
+
+                                            case 'd':
+                                                chNew = 'j';
+                                                break;
+                                            
+                                            case 'R':
+                                                chNew = 'L';
+                                                break;
+
+                                            case 'J':
+                                                chNew = 'D';
+                                                break;
+
+                                            case 'L':
+                                                chNew = 'R';
+                                                break;
+
+                                            case 'D':
+                                                chNew = 'J';
+                                                break;
+                                            
+                                            default: 
+                                                chNew = ch;
+                                        }
+                                        arText.push(chNew);
+                                    }
+                                    let strText = arText.join('');
+                                    conn.sendMessage(msg.key.remoteJid, strText, MessageType.text, {quoted:msg}).then((response) => {
+                                        console.log("Sent dual kiddo style message.");
+                                    }).catch(msgSendError);
+                                }
+                                else {
+                                    conn.sendMessage(msg.key.remoteJid, "*BEWAbot:* Tag a text message!", MessageType.text).then((response) => console.log("Message rejected: Non-text message tagged")).catch(msgSendError);
+                                }
+                                break;
+                            
+                            case "toImg": 
+                                let stickMsgType = Object.keys(msg.message.extendedTextMessage.contextInfo.quotedMessage)[0];
+                                if(stickMsgType === MessageType.sticker) {
+                                    console.log("Received request for converting sticker to image.")
+                                    let messId = msg.message.extendedTextMessage.contextInfo.stanzaId;
+                                    conn.loadMessage(msg.key.remoteJid, messId).then((origSticker) => {
+                                        conn.downloadMediaMessage(origSticker, "stream").then((imgStream) => {
+                                            gm(imgStream).write("stToImg.png", (err) => {
+                                                if(err) {console.log("ERROR: " + err);}
+                                                else {
+                                                    console.log("Converted sticker to image")
+                                                    conn.sendMessage(msg.key.remoteJid, fs.readFileSync('./stToImg.png'), MessageType.image, {quoted:msg, mimetype:Mimetype.png}).then((response) => {
+                                                        console.log("Message sent");
+                                                        fs.unlink("./stToImg.png", (err) => {
+                                                            if(err) {console.log("Error in deleting image: " + err);}
+                                                        });
+                                                    }).catch((err) => {
+                                                        console.log("Error in sending image of sticker: " + err);
+                                                    });
+                                                }
+                                            });
+                                        });
+                                    })
+                                }
+                                else {
+                                    conn.sendMessage(msg.key.remoteJid, "*BEWAbot:* Tag a sticker!", MessageType.text).then((response) => console.log("Message rejected: Non-sticker message tagged")).catch(msgSendError);
                                 }
                         }
                     }
